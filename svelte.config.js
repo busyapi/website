a97e2ee29069824979fe5b995c705801660fef79
busyapi <contact@busyapi.dev>
2023-08-05 00:25:22 +0200

    Update CSP configuration

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,20 @@ const config = {
 				include: ['/*'],
 				exclude: ['<all>']
 			}
-		})
+		}),
+		csp: {
+			directives: {
+				'script-src': ['self', 'https://*.googletagmanager.com', 'nonce-gtag'],
+				'style-src': ['self', 'https://fonts.googleapis.com'],
+				'img-src': ['self', 'https://*.google-analytics.com', 'https://*.googletagmanager.com'],
+				'connect-src': [
+					'self',
+					'https://*.google-analytics.com',
+					'https://*.analytics.google.com',
+					'https://*.googletagmanager.com'
+				]
+			}
+		}
 	}
 };
 
