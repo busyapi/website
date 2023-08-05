bec4e9ee0331a86d6f23e42a9d041056f12a23b4
busyapi <contact@busyapi.dev>
2023-08-05 13:37:29 +0200

    Add HSTS header

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,7 @@ export function load({ setHeaders }) {
 	setHeaders({
 		'X-Content-Type-Options': 'nosniff',
 		'X-Frame-Options': 'SAMEORIGIN',
-		'Referrer-Policy': 'strict-origin-when-cross-origin'
+		'Referrer-Policy': 'strict-origin-when-cross-origin',
+		'Strict-Transport-Security': 'max-age=31536000 includeSubDomains'
 	});
 }
